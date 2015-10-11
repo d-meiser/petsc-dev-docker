cfbf5146e49e9824b16f5a3b7c5001c242ebf00e
Fabio C. Canesin <fabio.canesin@gmail.com>
2015-10-11 18:43:32 -0300

    simplify for new centos based openblas-git container

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM canesin/openblas-git:latest
 MAINTAINER Fabio Cesar Canesin, fabio.canesin@gmail.com
 
-ENV DEBIAN_FRONTEND noninteractive
-
 # Install PETSc from latest stable
 ADD petsc.sh /petsc.sh
 ADD http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.6.tar.gz /tmp/petsc.tar.gz
--- petsc.sh
+++ petsc.sh
@@ -7,7 +7,7 @@ tar -xzf petsc.tar.gz
 rm petsc.tar.gz
 cd `ls | grep petsc`
 
-python3 configure PETSC_ARCH=dorcker-gnu-opt \
+./configure PETSC_ARCH=dorcker-gnu-opt \
             --prefix=/opt/petsc \
             --with-blas-lib=/opt/OpenBLAS/libopenblas.so \
             --with-lapack-lib=/opt/OpenBLAS/libopenblas.so \
