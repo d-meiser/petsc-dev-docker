6d1eeb14a9755394fa150a004b974191a274b662
Fabio C. Canesin <fabio.canesin@gmail.com>
2015-10-11 22:51:05 -0300

    use blas-lapack switch

--- petsc.sh
+++ petsc.sh
@@ -9,8 +9,7 @@ cd `ls | grep petsc`
 
 ./configure PETSC_ARCH=dorcker-gnu-opt \
             --prefix=/opt/petsc \
-            --with-blas-lib=/opt/OpenBLAS/libopenblas.so \
-            --with-lapack-lib=/opt/OpenBLAS/libopenblas.so \
+            --with-blas-lapack-dir=/opt/OpenBLAS/libopenblas.so \
             --download-mpich \
             --download-superlu_dist \
             --download-metis \
